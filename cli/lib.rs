8383946428e2775128fdbb2425f6df5a477f8263
Carlos Galarza <carloslfu@gmail.com>
2024-12-03 22:10:48 -0500

    fix

--- cli/lib.rs
+++ cli/lib.rs
@@ -62,7 +62,7 @@ fn op_my_fn() -> Option<String> {
 deno_runtime::deno_core::extension!(
   my_extension,
   ops = [op_my_fn],
-  esm_entry_point = "ext:my_extension/mod.js",
+  esm_entry_point = "ext:my_extension/my_extension.js",
   esm = ["my_extension.js"],
 );
 
